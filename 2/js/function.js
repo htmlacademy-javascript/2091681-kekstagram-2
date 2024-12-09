8b789b27144153f105c4de2817edff0f8c35aa9b
Keks <keks@htmlacademy.ru>
2024-12-09 21:29:30 +0300

    :heavy_check_mark: Сборка #2

---
+++ 2/js/function.js
@@ -0,0 +1,22 @@
+function checkLength(string = '', maxSymbol = 1) {
+  return (string.length <= maxSymbol);
+}
+
+console.log(checkLength('Javascript', 10));
+
+const isPolindrome = (string = '') => {
+
+  string = string.replaceAll(' ', '');
+  string = string.toLocaleLowerCase();
+
+  let result = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+  }
+
+  return string === result;
+};
+
+console.log(isPolindrome('abca'));
+
