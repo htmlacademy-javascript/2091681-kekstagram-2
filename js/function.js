c3cdd47a4ff90fa90956c03d8d3f83e4bdca34ed
Татьяна Баглаева <tsbaglaeva@mail.ru>
2025-01-09 17:09:53 +0500

    Добавляет функцию проверки длительности встречи в рабочее время

--- js/function.js
+++ js/function.js
@@ -52,3 +52,57 @@ const transformNumber = function (number) {
 transformNumber(0.12);
 
 
+// Проверяет, что встреча не выходит за рамки рабочего дня
+
+const getDateTime = (time) => {
+
+  const date = new Date();
+
+  const array = time.replaceAll(' ', '').split(':');
+
+  date.setHours(array[0], 10);
+  date.setMinutes(array[1], 10);
+  date.setSeconds(0);
+  date.setMilliseconds(0);
+
+  return date;
+};
+
+// console.log(getDateTime('15:20:30'));
+
+const checkMeetingTime = (dayStart, dayEnd, meetingStart, meetingDuration) => {
+  if (meetingDuration <= 0) {
+    return false;
+  }
+
+  const workTimeBegin = getDateTime(dayStart);
+  const workTimeEnd = getDateTime(dayEnd);
+
+  if (workTimeBegin > workTimeEnd) {
+    return false;
+  }
+
+  const meetTimeBegin = getDateTime(meetingStart);
+  const meetTimeEnd = new Date(meetTimeBegin);
+  meetTimeEnd.setMinutes(meetTimeEnd.getMinutes() + meetingDuration);
+
+  // console.log(workTimeBegin);
+  // console.log(workTimeEnd);
+  // console.log(meetTimeBegin);
+  // console.log(meetTimeEnd);
+  // console.log((meetTimeBegin >= workTimeBegin) && (meetTimeEnd <= workTimeEnd));
+
+  return ((meetTimeBegin >= workTimeBegin) && (meetTimeEnd <= workTimeEnd));
+};
+
+
+checkMeetingTime('08:00', '17:30', '14:00', 90); // true
+
+checkMeetingTime('8:0', '10:0', '8:0', 120); // true
+
+checkMeetingTime('08:00', '14:30', '14:00', 90); // false
+
+checkMeetingTime('14:00', '17:30', '08:0', 90); // false
+
+checkMeetingTime('8:00', '17:30', '08:00', 900); // false
+
