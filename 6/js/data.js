ad448e38f580428101d5164069f8c3ed0ac897fc
Keks <keks@htmlacademy.ru>
2025-01-13 21:54:27 +0300

    :heavy_check_mark: Сборка #6

---
+++ 6/js/data.js
@@ -0,0 +1,71 @@
+import {getRandomInt} from './util.js';
+import {getId} from './util.js';
+
+
+const PHOTO_COUNT = 25;
+
+const MIN_LIKES = 15;
+const MAX_LIKES = 200;
+
+const MIN_AVATAR_ID = 1;
+const MAX_AVATAR_ID = 6;
+
+const MIN_COMMENT_ID = 0;
+const MAX_COMMENT_ID = 30;
+
+const USER_COMMENTS = [
+  'Всё отлично!',
+  'В целом всё неплохо. Но не всё.',
+  'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
+  'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
+  'Я поскользнулся на банановой кожуре и уронил фотоаппарат на кота и у меня получилась фотография лучше.',
+  'Лица у людей на фотке перекошены, как будто их избивают. Как можно было поймать такой неудачный момент?!'
+];
+
+const USER_NAMES = [
+  'Анна',
+  'Михаил',
+  'Елизавета',
+  'Антон',
+  'Татьяна',
+  'Алексей',
+  'Мария',
+  'Константин',
+  'Надежда',
+  'Сергей',
+];
+
+const PHOTO_DESCRIPTION = ['Пляж', 'Указатель на пляж', 'Берег', 'Девушка в купальнике', 'Две миски супа', 'Автомобиль',
+  'Клубника на тарелке', 'Морс', 'Самолет над пляжем', 'Обувница', 'Дорожка на пляж', 'Серый Ауди', 'Овощная терелка',
+  'Сушикот', 'Сапоги робота', 'Самолет над облаками', 'Хор', 'Ретро автомобиль', 'Тапочки с фонариком',
+  'Пальмы у отеля', 'Рагу', 'Закат', 'Краб', 'Концерт', 'Land Rover'
+];
+
+const getCommentId = getId();
+
+const getPhotoId = getId();
+
+const getComment = () => ({
+  id: getCommentId(),
+  avatar: `img/avatar-${getRandomInt(MIN_AVATAR_ID, MAX_AVATAR_ID)}`,
+  message: USER_COMMENTS[getRandomInt(0, USER_COMMENTS.length - 1)],
+  name: USER_NAMES[getRandomInt(0, USER_NAMES.length - 1)]
+});
+
+getComment();
+
+const getPhotoDescription = () => {
+  const id = getPhotoId();
+
+  return {
+    id,
+    url:`photo/${id}.jpg`,
+    description: PHOTO_DESCRIPTION[id - 1],
+    likes:getRandomInt(MIN_LIKES, MAX_LIKES),
+    comments: Array.from({length:getRandomInt(MIN_COMMENT_ID, MAX_COMMENT_ID)}, getComment)
+  };
+};
+
+const getPhotos = () => Array.from({length:PHOTO_COUNT}, getPhotoDescription);
+
+export {getPhotos};
---
+++ 6/js/function.js
@@ -0,0 +1,100 @@
+// Проверяет длину строки
+
+function checkLength(string, maxSymbol) {
+  return (string.length <= maxSymbol);
+}
+
+checkLength('Javascript', 10);
+
+
+// Проверяет, является ли строка палиндромом
+
+const checkPalindrome = (string) => {
+
+  string = string.replaceAll(' ', '').toLowerCase();
+
+  let result = '';
+
+  for (let i = string.length - 1; i >= 0; i--) {
+    result += string[i];
+  }
+
+  return string === result;
+};
+
+checkPalindrome('abca');
+
+
+// Извлекает числа из строки
+
+const getNumber = function (string) {
+
+  const foundNamber = string.replaceAll(/\D/g, '');
+
+  return parseInt(foundNamber, 10);
+};
+
+getNumber ('агент 007');
+
+
+// Преобразует число в целое положительное
+
+const transformNumber = function (number) {
+
+  const string = String(number);
+
+  const foundNamber = string.replaceAll(/\D/g, '');
+
+  return parseInt(foundNamber, 10);
+
+};
+
+
+transformNumber(0.12);
+transformNumber(0.120);
+
+
+// Проверяет, что встреча не выходит за рамки рабочего дня
+
+const getDateTime = (time) => {
+  const [hours, minutes] = time.split(':');
+  return Number(hours) * 60 + Number(minutes);
+};
+
+// console.log(getDateTime('20:20'));
+
+const checkMeetingTime = (dayStart, dayEnd, meetingStart, meetingDuration) => {
+  if (meetingDuration <= 0) {
+    return false;
+  }
+
+  const workTimeBegin = getDateTime(dayStart);
+  const workTimeEnd = getDateTime(dayEnd);
+
+  if (workTimeBegin > workTimeEnd) {
+    return false;
+  }
+
+  const meetTimeBegin = getDateTime(meetingStart);
+  const meetTimeEnd = meetTimeBegin + meetingDuration;
+
+  // console.log(workTimeBegin);
+  // console.log(workTimeEnd);
+  // console.log(meetTimeBegin);
+  // console.log(meetTimeEnd);
+  // console.log((meetTimeBegin >= workTimeBegin) && (meetTimeEnd <= workTimeEnd));
+
+  return ((meetTimeBegin >= workTimeBegin) && (meetTimeEnd <= workTimeEnd));
+};
+
+
+checkMeetingTime('08:00', '17:30', '14:00', 90); // true
+
+checkMeetingTime('8:0', '10:0', '8:0', 120); // true
+
+checkMeetingTime('08:00', '14:30', '14:00', 90); // false
+
+checkMeetingTime('14:00', '17:30', '08:0', 90); // false
+
+checkMeetingTime('8:00', '17:30', '08:00', 900); // false
+
